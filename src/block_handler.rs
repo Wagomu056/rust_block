234a60d11b1073418becd174996feea1e322d10b
Takuya Azumaguchi <catchballer@gmail.com>
2023-02-12 18:41:54 +0900

    Change to place some blocks

--- src/block_handler.rs
+++ src/block_handler.rs
@@ -1,3 +1,4 @@
+use alloc::vec::Vec;
 use anyhow::Error;
 use crankstart::graphics::{Graphics, rect_make};
 use crankstart::sprite::{Sprite, SpriteManager};
@@ -6,10 +7,13 @@ use crankstart_sys::{LCDBitmapFlip, PDButtons};
 use crankstart_sys::{LCD_COLUMNS, LCD_ROWS};
 use euclid::{vec2, Vector2D};
 
+const x_num : i32 = 7;
+const y_num : i32 = 5;
+const total_num : i32 = x_num * y_num;
+
 pub struct BlockHandler {
-    sprite: Sprite,
-    image_size: Vector2D<f32, f32>,
-    pos: Vector2D<f32, f32>,
+    sprites: Vec<Sprite>,
+    positions: Vec<Vector2D<f32, f32>>,
 }
 
 impl BlockHandler {
@@ -17,31 +21,46 @@ impl BlockHandler {
         let sprite_manager = SpriteManager::get_mut();
         let graphics = Graphics::get();
 
-        // setup player
-        let mut sprite = sprite_manager.new_sprite()?;
+        // load image
         let image = graphics.load_bitmap("assets/images/block")?;
         let image_data = image.get_data()?;
-        let image_size :Vector2D<f32, f32> = vec2(
+        let image_size : Vector2D<f32, f32>= vec2(
             image_data.width as f32,
             image_data.height as f32);
-        let cr = rect_make(
-            0.0, 0.0,
-            image_size.x, image_size.y
-        );
+        let half_image_size : Vector2D<f32, f32> = vec2(
+            image_size.x * 0.5,
+            image_size.y * 0.5);
 
-        sprite.set_image(image, LCDBitmapFlip::kBitmapUnflipped)?;
-        sprite.set_collide_rect(&cr)?;
+        let mut sprites: Vec<Sprite> = Vec::new();
+        let mut positions: Vec<Vector2D<f32, f32>> = Vec::new();
 
-        let center_x = 25.0 + image_size.x * 0.5;
-        let start_y = 25.0 + image_size.y * 0.5;
-        sprite.move_to(center_x, start_y)?;
-        sprite_manager.add_sprite(&sprite)?;
+        let start_x = 25.0;
+        let start_y = 25.0;
+        for y in 0..y_num {
+            for x in 0..x_num {
+                let mut sprite = sprite_manager.new_sprite()?;
+                let cr = rect_make(
+                    0.0, 0.0,
+                    image_size.x, image_size.y
+                );
+
+                sprite.set_image(image.clone(), LCDBitmapFlip::kBitmapUnflipped)?;
+                sprite.set_collide_rect(&cr)?;
+
+                let pos_x = start_x + image_size.x * x as f32 + half_image_size.x;
+                let pos_y = start_y + image_size.y * y as f32 + half_image_size.y;
+                sprite.move_to(pos_x, pos_y)?;
+                sprite_manager.add_sprite(&sprite)?;
+
+                sprites.push(sprite);
+                positions.push(vec2(pos_x, pos_y));
+            }
+        }
 
         Ok(
             BlockHandler {
-                sprite,
-                image_size,
-                pos: vec2(center_x, start_y),
+                sprites,
+                positions,
             }
         )
     }
